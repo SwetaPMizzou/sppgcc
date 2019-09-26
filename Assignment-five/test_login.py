1ade88cfff2ec7699e5683a7a4ed6596906f41a6
SwetaPMizzou <54452439+SwetaPMizzou@users.noreply.github.com>
2019-09-25 23:28:31 -0500

    Update test_login.py

--- Assignment-five/test_login.py
+++ Assignment-five/test_login.py
@@ -4,12 +4,12 @@ from instructorLogin import instructorLogin
 
 def test_login_success():
     """
-    The Admin user can login with a password value of 'password'
+    The instructor can login with a password value of 'password'
     """
     assert instructorLogin( 'instructor', 'instructor' ) == True
 
 def test_login_fail():
     """
-    The Admin user login will fail with an invalid password
+    The instructor login will fail with an invalid password
     """
     assert instructorLogin( 'instructor', 'student' ) == False
